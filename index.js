aa280b0e58065867adfde9d16ed90d77795cc53f
magalhaess85 <igormagalhaess85@gmail.com>
2023-11-09 18:24:24 -0400

    Instalação do Express e aplicação em um localhost.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Olá Mundo!");
+const express = require("express");
+const app = express();
+
+app.get("/", function(req, res){
+    res.send("Olá, Mundo!");
+});
+
+app.get("/hello", function(req, res){
+    res.send("Hello, World!");
+});
+
+app.listen(3000, console.log("Servidor rodando em http://localhost:3000"));
