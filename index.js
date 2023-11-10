0894d489a2d0a0b63ae82a2a8035bb6348674867
Igor Soares Magalhães <igormagalhaess85@gmail.com>
2023-11-10 19:15:27 -0400

    Criação do endpoint 'Read by ID - [GET] /item/:i

--- index.js
+++ index.js
@@ -15,8 +15,18 @@ app.get("/hola", function(req, res){
 
 const lista = ["Rick Sanchez", "Morty Smith", "Summer Smith"]
 
+// Read ALL - [GET] /item 
 app.get("/item", function(req, res){
     res.send(lista);
+});
+
+// Read by ID - [GET] /item/:id
+app.get("/item/:id", function(req, res){
+    const id = req.params.id - 1;
+
+    const item = lista[id];
+
+    res.send(item);
 });
 
 app.listen(3000, console.log("Servidor rodando em http://localhost:3000"));
