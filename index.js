4c9df8d543752917f43712b5b6782438642f639d
Igor Soares Magalhães <igormagalhaess85@gmail.com>
2023-11-11 00:16:44 -0400

    Instalação da extensão 'Prettier' no projeto.

--- index.js
+++ index.js
@@ -7,78 +7,78 @@ const app = express();
 // JSON.parse para o conteúdo recebido.
 app.use(express.json());
 
-app.get("/", function(req, res){
-    res.send("Olá, Mundo!");
+app.get("/", function (req, res) {
+  res.send("Olá, Mundo!");
 });
 
-app.get("/hello", function(req, res){
-    res.send("Hello, World!");
+app.get("/hello", function (req, res) {
+  res.send("Hello, World!");
 });
 
-app.get("/hola", function(req, res){
-    res.send("¡Hola, Mundo!");
+app.get("/hola", function (req, res) {
+  res.send("¡Hola, Mundo!");
 });
 
-const lista = ["Rick Sanchez", "Morty Smith", "Summer Smith"]
+const lista = ["Rick Sanchez", "Morty Smith", "Summer Smith"];
 
-// Read ALL - [GET] /item 
-app.get("/item", function(req, res){
-    res.send(lista.filter(Boolean));
+// Read ALL - [GET] /item
+app.get("/item", function (req, res) {
+  res.send(lista.filter(Boolean));
 });
 
 // Read by ID - [GET] /item/:id
-app.get("/item/:id", function(req, res){
-    // Pegamos  o ID de rota e subtraímos 1 para ficar
-    // equivalente ao índice da lista que começa em 0.
-    const id = req.params.id - 1;
+app.get("/item/:id", function (req, res) {
+  // Pegamos  o ID de rota e subtraímos 1 para ficar
+  // equivalente ao índice da lista que começa em 0.
+  const id = req.params.id - 1;
 
-    // Acessamos o item da lista, usando o índice corrigido,
-    const item = lista[id];
+  // Acessamos o item da lista, usando o índice corrigido,
+  const item = lista[id];
 
-    // Enviamos o item como resposta do endpoint.
-    res.send(item);
+  // Enviamos o item como resposta do endpoint.
+  res.send(item);
 });
 
-// Create - [POST]/item 
-app.post("/item", function(req, res){
-    // Extraímos o nome do Body da requisição.
-    const item = req.body.nome;
+// Create - [POST]/item
+app.post("/item", function (req, res) {
+  // Extraímos o nome do Body da requisição.
+  const item = req.body.nome;
 
-    // Adicionamos o item recebido na lista.
-    lista.push(item);
+  // Adicionamos o item recebido na lista.
+  lista.push(item);
 
-    // Exibimos uma mensagem de sucesso.
-    res.send("Item adicionado com sucesso!");
+  // Exibimos uma mensagem de sucesso.
+  res.send("Item adicionado com sucesso!");
 });
 
 // Update - [PUT] /item
-app.put("/item/:id", function(req, res){
-    // Obtemos o ID do parâmetro de rota e fazemos
-    // a correção do índice.
-    const id = req.params.id - 1;
+app.put("/item/:id", function (req, res) {
+  // Obtemos o ID do parâmetro de rota e fazemos
+  // a correção do índice.
+  const id = req.params.id - 1;
 
-    // Obtemos o novo índice a ser atualizado.
-    const novoItem = req.body.nome;
+  // Obtemos o novo índice a ser atualizado.
+  const novoItem = req.body.nome;
 
-    // Atualizamos o valor recebido na lista, usando 
-    // a posição ID para garantir que atualizamos
-    // o item correto.
-    lista[id] = novoItem;
+  // Atualizamos o valor recebido na lista, usando
+  // a posição ID para garantir que atualizamos
+  // o item correto.
+  lista[id] = novoItem;
 
-    // Enviamos uma mensagem de sucesso.
-    res.send("Item atualizado com sucesso!");
+  // Enviamos uma mensagem de sucesso.
+  res.send("Item atualizado com sucesso!");
 });
 
 // Delete - [DELETE] /item/:id
-app.delete("/item/:id", function(req, res){
-    // Obtemos o ID do parâmetro de rota.
-    const id = req.params.id - 1;
+app.delete("/item/:id", function (req, res) {
+  // Obtemos o ID do parâmetro de rota.
+  const id = req.params.id - 1;
 
-    // Removemos o item da lista
-    delete lista[id];
+  // Removemos o item da lista
+  delete lista[id];
 
-    // Enviamos uma mensagem de sucesso
-    res.send("Item removido com sucesso!");
+  // Enviamos uma mensagem de sucesso
+  res.send("Item removido com sucesso!");
 });
 
-app.listen(3000, console.log("Servidor rodando em http://localhost:3000")); 
+app.listen(3000, console.log("Servidor rodando em http://localhost:3000"));
